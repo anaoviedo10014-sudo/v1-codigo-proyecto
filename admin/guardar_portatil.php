9618e2db5c2599434e462ae8907f2a22dcf1fda4
Dylan <Dylancalero81@gmail.com>
2026-09-17 15:06:46 -0500

    Arreglo en las secciones de guardar

--- admin/guardar_portatil.php
+++ admin/guardar_portatil.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-if (!isset($_SESSION['usuario_id']) || $_SESSION['rol'] !== 'admin') {
+if (!isset($_SESSION['usuario_id']) || ($_SESSION['rol'] !== 'admin' && $_SESSION['rol'] !== 'administrador')) {
     header('Location: ../index.php');
     exit;
 }
